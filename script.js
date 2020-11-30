1c03a752d98070a6fa10c6dee75d04c707d9168c
Ethan Philpott <ephilpott2004@gmail.com>
2020-11-30 13:55:51 -0700

    Only 2 Commits? My bad.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 "use strict";
 
+// Sound effects
+
+let click = new Audio('audio/click.mp3');
+click.volume = .75;
+
+let wrong = new Audio("audio/wrong.mp3");
+wrong.volume = .50;
+
+let win  = new Audio("audio/win.mp3");
+win.volume = .80;
+
+let lose = new Audio("audio/lose.mp3");
+lose.playbackRate = 2;
+
 // Custom Cursor Code
 
 class Cursor {
@@ -51,7 +65,7 @@ class Cursor {
     }
 }
 
-let cursor = new Cursor(document.getElementById('cursor'), 0.025, 1);
+let cursor = new Cursor(document.getElementById('cursor'), 0.2, 1);
 
 // had a hover effect to anything with the class "hover"
 
@@ -62,6 +76,9 @@ for (let i = 0; i < els.length; i++) {
     })
     els[i].addEventListener('mouseleave', () => {
         cursor.Unhover();
+    })
+    els[i].addEventListener('click', () => {
+        click.play();
     })
 }
 
@@ -114,12 +131,17 @@ function ChangeTheme (colors) {
 let turn = 0;
 let board = [,,,,,,,,,];
 
-// Switches the turns as the buttons are clicked by the player also does a cool fade in animation!
-
-let boxes = document.getElementsByClassName('b')
+let boxes = document.getElementsByClassName('b');
+let p1Score = document.getElementById("p1-score");
+let p2Score = document.getElementById("p2-score");
 
 for (let b of boxes) {
-    b.addEventListener('click', (e) => {
+    b.addEventListener('click', Select)
+}
+
+let canClick = true;
+function Select (e) {
+    if (canClick) {    
         let button = e.path[0];
         if (!board[button.value]) {
             let opacity = 0;
@@ -128,7 +150,7 @@ for (let b of boxes) {
                 let x = button.childNodes[1];
                 x.style.display = "block";
                 let id = setInterval(() => {
-                    if (opacity == 1) {
+                    if (opacity >= 1) {
                         clearInterval(id);
                     } else {
                         x.style.opacity = opacity;
@@ -140,7 +162,7 @@ for (let b of boxes) {
                 let o = button.childNodes[3];
                 o.style.display = "block";
                 let id = setInterval(() => {
-                    if (opacity == 1) {
+                    if (opacity >= 1) {
                         clearInterval(id);
                     } else {
                         o.style.opacity = opacity;
@@ -148,26 +170,29 @@ for (let b of boxes) {
                     }
                 }, 5)
             }
-            let cw = CanWin();
+            let cw = CanWin(board);
             if (cw[0] === 1) {
-                if (cw[1] > 0) {
-                    Win('x');
+                if (cw[1] < 0) {
+                    Win('x', cw[2]);
                 } else {
-                    Win('o');
+                    Win('o', cw[2]);
                 }
             } else if (cw[0] === -1) {
                 Win('tie');
             } else {
                 Turn();
             }
+        } else {
+            wrong.play();
         }
-    })
+    }
 }
 
 function Turn () {
     if (difficulty === 'Player') {
         turn++;
     } else if (difficulty === "Easy") {
+        // Picks a random, empty place on the board
         let free = [];
         for (let i = 0; i < board.length; i++) {
             if (board[i] == undefined) {
@@ -181,7 +206,7 @@ function Turn () {
             buttonNum = Math.floor(Math.random() * 9)
         }
 
-        let button = document.getElementsByClassName('b')[buttonNum]
+        let button = boxes[buttonNum]
 
         board[button.value] = 1;
         let opacity = 0;
@@ -189,7 +214,7 @@ function Turn () {
 
         o.style.display = "block";
         let id = setInterval(() => {
-            if (opacity == 1) {
+            if (opacity >= 1) {
                 clearInterval(id);
             } else {
                 o.style.opacity = opacity;
@@ -197,25 +222,53 @@ function Turn () {
             }
         }, 5)
     } else if (difficulty === "Hard") {
-        // Does random 25% of time, does optimal 75% of the time
+        let buttonNum = MiniMax(board, 0, true);
+        console.log(buttonNum)
+        let button = boxes[buttonNum]
+
+        board[button.value] = 1;
+        let opacity = 0;
+        let o = button.childNodes[3];
+
+        o.style.display = "block";
+        let id = setInterval(() => {
+            if (opacity >= 1) {
+                clearInterval(id);
+            } else {
+                o.style.opacity = opacity;
+                opacity += 0.01;
+            }
+        }, 5)
     } else if (difficulty === 'Impossible') {
         // Chooses optimally 100% if the time
     }
-    let cw = CanWin();
+    let cw = CanWin(board);
     if (cw[0] === 1) {
-        if (cw[1] > 0) {
-            Win('x');
+        if (cw[1] < 0) {
+            Win('x', cw[2]);
         } else {
-            Win('o');
+            Win('o', cw[2]);
         }
-        setTimeout (Reset, 1000);
     } else if (cw[0] === -1) {
         Win('tie');
-        setTimeout (Reset, 1000);
     }
 }
 
-function CanWin () {
+function MiniMax (tempBoard, depth, pturn) {
+    // console.log(tempBoard)
+    let cw = CanWin(tempBoard);
+    if (cw[0] === 1) {
+        if (cw[1] === -1) {
+            return -1
+        } else {
+            return 1
+        }
+    } else if (cw[0] === -1) {
+        return 0;
+    } 
+}
+
+function CanWin (tempBoard) {
     let wincon = [
         [0, 1, 2],
         [3, 4, 5],
@@ -228,13 +281,13 @@ function CanWin () {
     ]
 
     for (let win of wincon) {
-        if (board[win[0]] !== undefined && (board[win[0]] === board[win[1]] && board[win[1]] === board[win[2]])) {
-            return [1, board[win[0]]]
+        if (tempBoard[win[0]] !== undefined && (tempBoard[win[0]] === tempBoard[win[1]] && tempBoard[win[1]] === tempBoard[win[2]])) {
+            return [1, tempBoard[win[0]], win]
         }
     }
 
     let hasUndef = false;
-    for (let box of board) {
+    for (let box of tempBoard) {
         if (box == undefined) {
             hasUndef = true;
             break;
@@ -248,16 +301,114 @@ function CanWin () {
     }
 }
 
-function Win (who) {
-    Reset();
+function Win (who, play=0) {
+    canClick = false;
+
+    if (who === 'tie') {
+        lose.play();
+    } else {
+        win.play();
+    } 
+
+    setTimeout(() => {
+        if (play) {
+            let bor = document.getElementById('board');
+            let centerX = bor.offsetLeft + bor.offsetWidth / 2;
+            let centerY = bor.offsetTop + bor.offsetHeight / 2;
+            let amt = 0;
+            let xOffT = [];
+            let xOffL = [];
+            let oOffT = [];
+            let oOffL = [];
+            let middle = document.getElementById('middle');
+
+            for (let value of play) {
+                let butt = boxes[value];
+                let x = butt.childNodes[1];
+                let o = butt.childNodes[3];
+                
+                xOffT.push(x.offsetTop  + x.offsetHeight / 2);
+                xOffL.push(x.offsetLeft + x.offsetWidth  / 2);
+                oOffT.push(o.offsetTop  + o.offsetHeight / 2);
+                oOffL.push(o.offsetLeft + o.offsetWidth  / 2);
+            }
+
+            let id = setInterval(() => {
+                if (amt >= 1) {
+                    clearInterval(id)
+                } else {
+                    for (let i = 0; i < play.length; i++) {
+                        let butt = boxes[play[i]];
+                        let x = butt.childNodes[1];
+                        let o = butt.childNodes[3];
+                        
+                        x.style.position = "relative";
+                        o.style.position = "relative";
+
+                        x.style.top  = (centerY - xOffT[i]) * amt + 'px';
+                        x.style.left = (centerX - xOffL[i]) * amt + 'px';
+                        o.style.top  = (centerY - oOffT[i]) * amt + 'px';
+                        o.style.left = (centerX - oOffL[i]) * amt + 'px';
+                    }
+                    if (!play.includes(4)) {
+                        middle.style.opacity = 1 - amt;
+                    }
+                    amt += 0.02;
+                }
+            }, 10)
+        }
+
+        setTimeout (() => {
+            Reset();
+            if (who !== 'tie') {
+                setTimeout (() => {
+                    for (let value of play) {
+                        let butt = boxes[value];
+                        let x = butt.childNodes[1];
+                        let o = butt.childNodes[3];
+                        
+                        x.style.position = "block";
+                        o.style.position = "block";
+            
+                        x.style.top  = '0px';
+                        x.style.left = '0px';
+                        o.style.top  = '0px';
+                        o.style.left = '0px';
+                    }
+                }, 1000);
+            }
+            canClick = true;
+            
+        }, 1000);
+    }, 1000);
+
+    if (who === 'x') {
+        p1Score.textContent = Number(p1Score.textContent) + 1;
+    } else if (who === 'o') {
+        p2Score.textContent = Number(p2Score.textContent) + 1;
+    }
 }
 
 function Reset () {
-    
-    
-
-    turn = 0;
-    board = [,,,,,,,,,];
+    document.getElementById('middle').style.opacity = 1;
+    let opacity = 1;
+    let id = setInterval (() => {
+        if (opacity <= 0) {
+            for (let box of boxes) {
+                box.childNodes[1].style.display = "none";
+                box.childNodes[3].style.display = "none";
+            }
+            turn = 0;
+            board = [,,,,,,,,,];
+            clearInterval(id)
+        } else {
+            for (let box of boxes) {
+                box.childNodes[1].style.opacity = opacity;
+                box.childNodes[3].style.opacity = opacity;
+            }
+            opacity -= 0.01;
+        }
+    }, 5) 
 }
 
 
